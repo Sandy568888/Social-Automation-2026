3ce8ef34fad95bdef619899a969e13feac5ee9ff
ATERE EMMANUEL OLUWASEYI <emmanuelatere44@gmail.com>
2026-08-20 14:40:07 +0000

    fix: remove duplicate brace in top menu

--- apps/frontend/src/components/layout/top.menu.tsx
+++ apps/frontend/src/components/layout/top.menu.tsx
@@ -73,7 +73,6 @@ export const useMenuItem = () => {
       path: '/agents',
     },
     {
-    {
       name: 'Automation',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
